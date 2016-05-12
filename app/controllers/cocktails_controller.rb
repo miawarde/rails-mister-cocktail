1eac5a198b47d7fb8c8b8370af5711ea7f325154
Mia Warde <mia.maria.warde@gmail.com>
2016-05-12 16:53:06 +0200

    Mister Cocktail no CSS

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,16 +13,15 @@ class CocktailsController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
   end
 
   private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
-  end
-
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
   end
 
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,36 @@
 class DosesController < ApplicationController
+
+  def new
+    @ingredients = Ingredient.all
+    @cocktail = find_cocktail
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    @dose.save
+    redirect_to cocktail_path(find_cocktail)
+  end
+
+  def destroy
+    @cocktail = find_cocktail
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def find_cocktail
+    Cocktail.find(params[:cocktail_id])
+  end
+
 end
+
+
+
